45489b818b92cd506c30fc043fd0edca9d76b231
FiveS <sgmmsionide@gmail.com>
2026-09-03 22:46:02 +1000

    Expose MiraCombat bounty API

--- src/main/java/com/mira/combat/api/MiraCombatApi.java
+++ src/main/java/com/mira/combat/api/MiraCombatApi.java
@@ -2,9 +2,13 @@ package com.mira.combat.api;
 
 import org.bukkit.entity.Player;
 
+import java.util.UUID;
+
 public interface MiraCombatApi {
     boolean enabled();
     double attackSpeed();
     int maximumNoDamageTicks();
     void refresh(Player player);
+    double bounty(UUID player);
+    boolean bountiesAvailable();
 }
