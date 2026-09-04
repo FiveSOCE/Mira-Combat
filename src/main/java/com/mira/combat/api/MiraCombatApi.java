e8a885dc8f22a796d0a11185b00f411e098f4ffb
FiveS <sgmmsionide@gmail.com>
2026-09-04 17:55:53 +1000

    Remove bounty methods from MiraCombat API

--- src/main/java/com/mira/combat/api/MiraCombatApi.java
+++ src/main/java/com/mira/combat/api/MiraCombatApi.java
@@ -2,13 +2,9 @@ package com.mira.combat.api;
 
 import org.bukkit.entity.Player;
 
-import java.util.UUID;
-
 public interface MiraCombatApi {
     boolean enabled();
     double attackSpeed();
     int maximumNoDamageTicks();
     void refresh(Player player);
-    double bounty(UUID player);
-    boolean bountiesAvailable();
 }
